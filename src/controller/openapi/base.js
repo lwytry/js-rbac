cd897ca0a32ef87ed1b32dad5d50f2a5d31dfc0a
lwy <lwy@Lwys-MacBook-Pro.local>
2018-12-26 13:55:16 +0800

    role

--- src/controller/openapi/base.js
+++ src/controller/openapi/base.js
@@ -1,19 +1,25 @@
 module.exports = class extends think.Controller {
   constructor(ctx) {
     super(ctx);
-    ctx.ProjectId = this.getProjectId(ctx);
+    ctx.ProjectId = this.getProjectId();
   }
   __before() {
   }
-  async getProjectId(ctx) {
-    let projectRequestId = ctx.param('projectRequestId');
+  async getProjectId() {
+    let projectRequestId = this.get('projectRequestId');
+    if (think.isEmpty(projectRequestId)) {
+      projectRequestId = this.post('projectRequestId');
+    }
+    if (think.isEmpty(projectRequestId)) {
+      return this.fail(1, 'param invalid');
+    }
     let projectId = await this.cache(projectRequestId, undefined, 'redis');
 
     if (think.isEmpty(projectId)) {
       const ProjectRep = think.service('projectRep');
       let result = await ProjectRep.getInfoByRequestId(requestId);
       if (think.isEmpty(result.id)) {
-        return this.fail(1, 'projectID error');
+        return this.fail(2, 'projectID error');
       }
       projectId = result.id;
       await this.cache(requestId, result.id, {
--- src/logic/openapi/role.js
+++ src/logic/openapi/role.js
@@ -45,10 +45,6 @@ module.exports = class extends think.Logic {
   }
   addAction() {
     let rules = {
-      projectRequestId: {
-        string: true,       // 字段类型为 String 类型
-        required: true,     // 字段必填
-      },
       userId: {
         int: true,       // 字段类型为 String 类型
         required: true,     // 字段必填
